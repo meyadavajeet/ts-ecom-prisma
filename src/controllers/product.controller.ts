8f9f4be646d69fa3228d3085035bbe341508ba3a
Ajeet Atpl <ajeet.yadav@atpl.live>
2025-04-10 08:46:04 +0530

    after product controller updated

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { ProductSchema } from "../schema/product";
 import { prismaClient } from "../app";
+import { NotFoundException } from "../exceptions/notfound.exceptions";
+import { ErrorCode } from "../exceptions/root.exceptions";
 
 interface CreateProductRequestBody {
   name: string;
@@ -22,4 +24,61 @@ export const createProduct = async (
     },
   });
   res.status(201).json(product);
+};
+
+export const getAllProducts = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const products = await prismaClient.product.findMany();
+  res.status(200).json(products);
+};
+
+export const getProductById = async (
+  req: Request<{ id: number }, {}, {}>,
+  res: Response
+): Promise<void> => {
+  const { id } = req.params;
+  if (!id) {
+    throw new NotFoundException("Product not found", ErrorCode.NOT_FOUND);
+  }
+  const product = await prismaClient.product.findFirst({
+    where: { id },
+  });
+  if (!product) {
+    throw new NotFoundException("Product not found", ErrorCode.NOT_FOUND);
+  }
+  res.status(200).json(product);
+};
+
+export const deleteProductById = async (
+  req: Request<{ id: number }, {}, {}>,
+  res: Response
+): Promise<void> => {
+  const { id } = req.params;
+  if (!id) {
+    throw new NotFoundException("Product not found", ErrorCode.NOT_FOUND);
+  }
+  const product = await prismaClient.product.delete({
+    where: { id },
+  });
+  res.status(200).json(product);
+};
+
+export const updateProduct = async (
+  req: Request<{ id: number }, {}, CreateProductRequestBody>,
+  res: Response
+): Promise<void> => {
+  const { id } = req.params;
+  if (!id) {
+    throw new NotFoundException("Product not found", ErrorCode.NOT_FOUND);
+  }
+  const product = await prismaClient.product.update({
+    where: { id },
+    data: {
+      ...req.body,
+      tags: req.body.tags.join(","),
+    },
+  });
+  res.status(200).json(product);
 };
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,14 +1,35 @@
 import { Router } from "express";
 import { errorHandler } from "../error.handler";
-import { createProduct } from "../controllers/product.controller";
+import { createProduct, deleteProductById, getAllProducts, getProductById, updateProduct } from "../controllers/product.controller";
 import authMiddleware from "../middlewares/auth.middleware";
 import adminMiddleware from "../middlewares/admin.middleware";
 const productRoutes: Router = Router();
 
 productRoutes.post(
-  "/create",
+  "/",
   [authMiddleware, adminMiddleware],
   errorHandler(createProduct)
+);
+
+productRoutes.get(
+  "/",
+  [authMiddleware, adminMiddleware],
+  errorHandler(getAllProducts)
+);
+productRoutes.get(
+  "/:id",
+  [authMiddleware, adminMiddleware],
+  errorHandler(getProductById)
+);
+productRoutes.put(
+  "/:id",
+  [authMiddleware, adminMiddleware],
+  errorHandler(updateProduct)
+);
+productRoutes.delete(
+  "/:id",
+  [authMiddleware, adminMiddleware],
+  errorHandler(deleteProductById)
 );
 
 export default productRoutes;
