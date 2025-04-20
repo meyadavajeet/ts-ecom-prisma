f13cc07b7344f58235bd096b7405dbc81050b907
Ajeet Atpl <ajeet.yadav@atpl.live>
2025-04-20 21:26:29 +0530

    after users and order routes and controllers

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -202,3 +202,105 @@ export const cancelOrder = async (req: Request, res: Response) => {
     res.status(200).json(order);
   });
 };
+
+/**
+ * ADMIN CONTROLLERS START
+ */
+
+export const listAllOrders = async (req: Request, res: Response) => {
+  let whereClause = {};
+  if (req.query.status) {
+    whereClause = {
+      ...whereClause,
+      status: req.query.status,
+    };
+  }
+  const orders = await prismaClient.order.findMany({
+    where: whereClause,
+    skip: req.query.skip ? +req.query.skip : 0,
+    take: req.query.take ? +req.query.take : 10,
+  });
+  res.status(200).json(orders);
+};
+
+export const changeStatus = async (req: Request, res: Response) => {
+  // start transaction
+  return await prismaClient.$transaction(async (prisma) => {
+    try {
+      // check if userId exist
+      if (!req?.user?.id) {
+        throw new UnAuthorizedException(
+          "User not authorized",
+          ErrorCode.UNAUTHORIZED,
+          "Please login to change order status"
+        );
+      }
+      if (!req.params.id) {
+        throw new UnAuthorizedException(
+          "Order ID not provided",
+          ErrorCode.BAD_REQUEST,
+          "Please provide order ID"
+        );
+      }
+      if (!req.body.status) {
+        throw new UnAuthorizedException(
+          "Status not provided",
+          ErrorCode.BAD_REQUEST,
+          "Please provide order status"
+        );
+      }
+      // change order status
+      const order = await prisma.order.update({
+        where: {
+          id: +req.params.id,
+        },
+        data: {
+          status: req.body.status,
+        },
+      });
+      // create order event
+      await prisma.orderEvent.create({
+        data: {
+          orderId: order.id,
+          status: req.body.status,
+        },
+      });
+      // return order
+      res.status(200).json(order);
+    } catch (error) {
+      console.error("Transaction error:", error);
+      return res.status(500).json({
+        message: "Internal server error",
+      });
+    }
+  });
+};
+
+export const listUsersOrders = async (req: Request, res: Response) => {
+  if (!req.query.userId) {
+    throw new UnAuthorizedException(
+      "User ID not provided",
+      ErrorCode.BAD_REQUEST,
+      "Please provide user ID"
+    );
+  }
+  let whereClause: any = {
+    userId: +req.query.userId,
+  };
+  if (req.query.status) {
+    whereClause = {
+      ...whereClause,
+      status: req.query.status,
+    };
+  }
+  const orders = await prismaClient.order.findMany({
+    where: whereClause,
+    skip: req.query.skip ? +req.query.skip : 0,
+    take: req.query.take ? +req.query.take : 10,
+  });
+  res.status(200).json(orders);
+};
+
+/**
+ * ADMIN CONTROLLERS END
+ */
--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -7,13 +7,22 @@ import {
   getOrderById,
   getAllOrders,
   cancelOrder,
+  listAllOrders,
+  listUsersOrders,
+  changeStatus,
 } from "../controllers/order.controller";
 import { errorHandler } from "../error.handler";
 import authMiddleware from "../middlewares/auth.middleware";
+import adminMiddleware from "../middlewares/admin.middleware";
 
 orderRouter.post("/", [authMiddleware], errorHandler(createOrder));
 orderRouter.get("/", [authMiddleware], errorHandler(getAllOrders));
-orderRouter.get("/:id", [authMiddleware], errorHandler(getOrderById));
 orderRouter.put("/:id/cancel", [authMiddleware], errorHandler(cancelOrder));
 
+// The order routes for the admin part with admin middleware
+orderRouter.get("/index", [authMiddleware, adminMiddleware], errorHandler(listAllOrders));
+orderRouter.get("/users", [authMiddleware, adminMiddleware], errorHandler(listUsersOrders));
+orderRouter.put("/:id/status", [authMiddleware, adminMiddleware], errorHandler(changeStatus));
+
+orderRouter.get("/:id", [authMiddleware], errorHandler(getOrderById));
 export default orderRouter;
