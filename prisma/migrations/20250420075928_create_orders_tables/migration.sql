bc2581b8df3168d0595ee3fc9d9a4f992667cdf9
Ajeet Atpl <ajeet.yadav@atpl.live>
2025-04-20 13:29:46 +0530

    orders table is created

---
+++ prisma/migrations/20250420075928_create_orders_tables/migration.sql
@@ -0,0 +1,49 @@
+-- CreateEnum
+CREATE TYPE "ORDER_EVENT_STATUS" AS ENUM ('PENDING', 'ACCEPTED', 'REJECTED', 'PROCESSING', 'PACKED', 'DISPATCHED', 'SHIPPED', 'OUT_FOR_DELIVERY', 'DELIVERED', 'CANCELLED', 'RETURNED');
+
+-- CreateTable
+CREATE TABLE "orders" (
+    "id" BIGSERIAL NOT NULL,
+    "userId" BIGINT NOT NULL,
+    "netAmount" DOUBLE PRECISION NOT NULL,
+    "shippingAddress" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "order_products" (
+    "id" BIGSERIAL NOT NULL,
+    "orderId" BIGINT NOT NULL,
+    "productId" BIGINT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "order_products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "order_events" (
+    "id" BIGSERIAL NOT NULL,
+    "orderId" BIGINT NOT NULL,
+    "status" "ORDER_EVENT_STATUS" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "order_events_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_products" ADD CONSTRAINT "order_products_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_products" ADD CONSTRAINT "order_products_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_events" ADD CONSTRAINT "order_events_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
